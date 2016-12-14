55a2ad6ad93f9ee43dcb4a1f5756206bf8bfa840
twilroad <269044570@qq.com>
2016-12-14 19:51:30 +0800

    move files to installer

---
+++ src/Controllers/InstallController.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-08-27 18:36
+ */
+namespace Notadd\Installer\Controllers;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Installer\Contracts\Prerequisite;
+use Notadd\Installer\Requests\InstallRequest;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+/**
+ * Class InstallController.
+ */
+class InstallController extends Controller
+{
+    /**
+     * @var \Notadd\Installer\Commands\InstallCommand
+     */
+    protected $command;
+
+    /**
+     * @var \Notadd\Installer\Contracts\Prerequisite
+     */
+    protected $prerequisite;
+
+    /**
+     * InstallController constructor.
+     *
+     * @param \Notadd\Installer\Contracts\Prerequisite $prerequisite
+     */
+    public function __construct(Prerequisite $prerequisite)
+    {
+        parent::__construct();
+        $this->prerequisite = $prerequisite;
+    }
+
+    /**
+     * TODO: Method index Description
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        $this->prerequisite->check();
+        $errors = $this->prerequisite->getErrors();
+        if (count($errors)) {
+            $this->share('errors', $errors);
+
+            return $this->view('install::errors');
+        } else {
+            return $this->view('install::install');
+        }
+    }
+
+    /**
+     * TODO: Method store Description
+     *
+     * @param \Notadd\Installer\Requests\InstallRequest $request
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function store(InstallRequest $request)
+    {
+        $this->command = $this->getCommand('install');
+        $this->command->setDataFromController($request->all());
+        $input = new ArrayInput([]);
+        $output = new BufferedOutput();
+        $this->command->run($input, $output);
+        $this->share('admin_account', $request->get('admin_account'));
+        $this->share('admin_email', $request->get('admin_email'));
+        $this->share('admin_password', $request->get('admin_password'));
+
+        return $this->view('install::success');
+    }
+}
---
+++ src/InstallerServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-10-24 10:49
+ */
+namespace Notadd\Installer;
+
+use Illuminate\Support\ServiceProvider;
+use Notadd\Installer\Commands\InstallCommand;
+use Notadd\Installer\Composite;
+use Notadd\Installer\Contracts\Prerequisite;
+use Notadd\Installer\Controllers\InstallController;
+use Notadd\Installer\Prerequisite\PhpExtension;
+use Notadd\Installer\Prerequisite\PhpVersion;
+use Notadd\Installer\Prerequisite\WritablePath;
+
+/**
+ * Class InstallServiceProvider.
+ */
+class InstallerServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot service provider.
+     */
+    public function boot()
+    {
+        if (!$this->app->isInstalled()) {
+            $this->app->make('router')->resource('/', InstallController::class);
+        }
+        $this->commands([
+            InstallCommand::class,
+        ]);
+        $this->loadViewsFrom(resource_path('views/install'), 'install');
+    }
+
+    /**
+     * Register for service provider.
+     */
+    public function register()
+    {
+        $this->app->bind(Prerequisite::class, function () {
+            return new Composite(new PhpVersion('5.6.28'), new PhpExtension([
+                'dom',
+                'fileinfo',
+                'gd',
+                'json',
+                'mbstring',
+                'openssl',
+                'pdo_mysql',
+            ]), new WritablePath([
+                public_path(),
+                storage_path(),
+            ]));
+        });
+    }
+}
