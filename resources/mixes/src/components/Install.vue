425e2fc7633dfd7be2b8d167cfb637110a1a117d
twilroad <269044570@qq.com>
2017-03-02 18:57:43 +0800

    add steps elements and styles

--- resources/mixes/src/components/Install.vue
+++ resources/mixes/src/components/Install.vue
@@ -2,3 +2,34 @@
   export default {
   }
 </script>
+<template>
+    <div class="install-wrap">
+        <div class="install-header">
+            <div class="container">
+                <span>当前版本：beta 1</span>
+            </div>
+        </div>
+        <div class="container">
+            <div class="install-content">
+                <div class="step-wrap">
+                    <div class="step success">
+                        <span>1</span>
+                        <em>检测环境</em>
+                    </div>
+                    <div class="step error">
+                        <span>2</span>
+                        <em>设置数据</em>
+                    </div>
+                    <div class="step active">
+                        <span>3</span>
+                        <em>设置账户</em>
+                    </div>
+                    <div class="step">
+                        <span>4</span>
+                        <em>获取结果</em>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
