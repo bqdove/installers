2045cdeb2fbf7ac853f0a85142627c4068720679
twilroad <269044570@qq.com>
2017-03-02 20:16:21 +0800

    add setting and account

--- resources/mixes/src/components/Install.vue
+++ resources/mixes/src/components/Install.vue
@@ -36,24 +36,92 @@
                             <div class="col-4">
                                 <div class="form-group">
                                     <label>您的网站名称</label>
-                                    <input type="text">
+                                    <input type="text" placeholder="admin">
                                 </div>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-12"></div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label>选择数据库</label>
+                                    <div class="btn-group">
+                                        <label class="active"><input type="radio">PostgreSQL</label>
+                                        <label><input type="radio">MySQL</label>
+                                        <label><input type="radio">SQLite3</label>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="row">
-                            <div class="col-4"></div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <label>数据库地址</label>
+                                    <input type="text" placeholder="admin">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <label>数据库用户名</label>
+                                    <input type="text" placeholder="admin">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <label>数据库名</label>
+                                    <input type="text" placeholder="admin">
+                                </div>
+                            </div>
                         </div>
                         <div class="row">
-                            <div class="col-4"></div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <label>账户</label>
+                                    <input type="text" placeholder="admin">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <label>密码</label>
+                                    <input type="text" placeholder="admin">
+                                </div>
+                            </div>
                         </div>
                         <div class="row">
-                            <div class="col-4"></div>
+                            <div class="col-4">
+                                <button type="submit">安装</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="step-content">
+                    <h1>填写账户设置</h1>
+                    <div class="form-horizontal">
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <label>管理员邮箱</label>
+                                    <input type="text" placeholder="admin">
+                                </div>
+                            </div>
                         </div>
                         <div class="row">
-                            <div class="col-4"></div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <label>账户</label>
+                                    <input type="text" placeholder="admin">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <label>密码</label>
+                                    <input type="text" placeholder="admin">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                <button type="submit">安装</button>
+                            </div>
                         </div>
                     </div>
                 </div>
