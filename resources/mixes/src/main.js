d28be4c1e8c3460ba81a7ee6bd0a0cc4bb67cc97
twilroad <269044570@qq.com>
2017-03-03 16:00:59 +0800

    using axios to instance http for Vue

--- resources/mixes/src/main.js
+++ resources/mixes/src/main.js
@@ -1,12 +1,17 @@
 import './assets/style.less'
 import Vue from 'vue'
 import App from './App'
+import notadd from './notadd'
 import router from './router'
 
+Vue.debug = true
 Vue.config.productionTip = false
+Vue.http = notadd.http
+Object.defineProperty(Vue.prototype, '$http', () => {
+  return notadd.http
+})
 
-/* eslint-disable no-new */
-new Vue({
+notadd.instance = new Vue({
   el: '#app',
   router,
   template: '<App/>',
---
+++ resources/mixes/src/notadd.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+axios.interceptors.response.use(response => response, error => {
+  console.log(error)
+  console.log(error.response)
+  console.log(error.response.data)
+})
+
+export default {
+  http: axios,
+  instance: null
+}
