6692fc2ff929004306ca1c19b51dd8cdd61b3b4d
twilroad <269044570@qq.com>
2017-03-03 17:19:07 +0800

    add logic for checking handler

--- src/Handlers/CheckingHandler.php
+++ src/Handlers/CheckingHandler.php
@@ -8,11 +8,76 @@
  */
 namespace Notadd\Installer\Handlers;
 
+use Illuminate\Container\Container;
 use Notadd\Foundation\Passport\Abstracts\DataHandler;
+use Notadd\Installer\Contracts\Prerequisite;
 
 /**
  * Class CheckingHandler.
  */
 class CheckingHandler extends DataHandler
 {
+    /**
+     * @var \Notadd\Installer\Contracts\Prerequisite
+     */
+    protected $prerequisite;
+
+    /**
+     * CheckingHandler constructor.
+     *
+     * @param \Illuminate\Container\Container          $container
+     * @param \Notadd\Installer\Contracts\Prerequisite $prerequisite
+     */
+    public function __construct(Container $container, Prerequisite $prerequisite)
+    {
+        parent::__construct($container);
+        $this->prerequisite = $prerequisite;
+    }
+
+    /**
+     * Http code.
+     *
+     * @return int
+     */
+    public function code()
+    {
+        return 200;
+    }
+
+    /**
+     * Errors for handler.
+     *
+     * @return array
+     */
+    public function errors()
+    {
+        return [
+            $this->translator->trans(''),
+        ];
+    }
+
+    /**
+     * Data for handler.
+     *
+     * @return array
+     */
+    public function data()
+    {
+        $this->prerequisite->check();
+        return [
+            'errors' => $this->prerequisite->getErrors(),
+        ];
+    }
+
+    /**
+     * Messages for handler.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            $this->translator->trans(''),
+        ];
+    }
 }
