8f73b460dc65efb2594f5ae7d0e24d9288013400
twilroad <269044570@qq.com>
2017-04-09 11:42:21 +0800

    add DatabaseHandler

---
+++ src/Handlers/DatabaseHandler.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-09 11:41
+ */
+namespace Notadd\Installer\Handlers;
+
+use Notadd\Foundation\Passport\Abstracts\SetHandler;
+
+/**
+ * Class DatabaseHandler.
+ */
+class DatabaseHandler extends SetHandler
+{
+
+}
