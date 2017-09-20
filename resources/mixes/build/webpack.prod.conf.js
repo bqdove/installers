e7672ef5a5f5ab347d955dbadbb3e92d49c861f4
twilroad <269044570@qq.com>
2017-09-20 10:49:20 +0800

    fix something

--- resources/mixes/build/webpack.prod.conf.js
+++ resources/mixes/build/webpack.prod.conf.js
@@ -1,16 +1,16 @@
-var path = require('path');
-var utils = require('./utils');
-var webpack = require('webpack');
-var config = require('../config');
-var merge = require('webpack-merge');
-var baseWebpackConfig = require('./webpack.base.conf');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+let path = require('path');
+let utils = require('./utils');
+let webpack = require('webpack');
+let config = require('../config');
+let merge = require('webpack-merge');
+let baseWebpackConfig = require('./webpack.base.conf');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 
-var env = config.build.env;
+let env = config.build.env;
 
-var webpackConfig = merge(baseWebpackConfig, {
+let webpackConfig = merge(baseWebpackConfig, {
     module: {
         rules: utils.styleLoaders({
             sourceMap: config.build.productionSourceMap,
@@ -82,7 +82,7 @@ var webpackConfig = merge(baseWebpackConfig, {
 });
 
 if (config.build.productionGzip) {
-    var CompressionWebpackPlugin = require('compression-webpack-plugin');
+    let CompressionWebpackPlugin = require('compression-webpack-plugin');
 
     webpackConfig.plugins.push(
         new CompressionWebpackPlugin({
@@ -100,7 +100,7 @@ if (config.build.productionGzip) {
 }
 
 if (config.build.bundleAnalyzerReport) {
-    var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+    let BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
     webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 }
 
