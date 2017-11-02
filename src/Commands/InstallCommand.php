18bfc3b068f52fec0f4bfe6f64a7e3951792c0e0
twilroad <269044570@qq.com>
2017-11-02 15:24:35 +0800

    switch queue driver to sync

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -237,7 +237,7 @@ class InstallCommand extends Command
         $database->put('REDIS_HOST', $this->data->get('redis_host'));
         $this->data->get('redis_password') && $database->put('REDIS_PASSWORD', $this->data->get('redis_password'));
         $database->put('REDIS_PORT', $this->data->get('redis_port'));
-        $database->put('QUEUE_DRIVER', 'redis');
+        $database->put('QUEUE_DRIVER', 'sync');
 
         file_put_contents($file, Yaml::dump($database->toArray()));
         touch($this->container->storagePath() . DIRECTORY_SEPARATOR . 'installed');
